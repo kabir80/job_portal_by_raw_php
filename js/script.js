ef7fb33b7d7bd9513d53633d25c03f5217f9553e
kabir80 <kabirmandaljob@gmail.com>
2021-03-09 12:29:57 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@ $(document).ready(function() {
 
     $("#signup").click(function(){
       var email = $("#email").val();
-      if(email == " "){
-          alert('input email');
-          return false;
+      var pass = $("#pass").val();
+      var pass1 = $("#pass1").val();
+    
+      $("#email").focusout(function(){check_email();});
+      function check_email(){
+          if(email == ""){
+              alert("input email");
+          }
       }
+    
        
     })
 
--- signup/index.php
+++ signup/index.php
@@ -13,7 +13,7 @@
 
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
+                <input type="email" name="email" class="form-control" id="email">
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
